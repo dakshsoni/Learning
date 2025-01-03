ebb11d42e24fa13a973492487558146e0cb2059d
deepak soni <46437145+dakshsoni@users.noreply.github.com>
2025-01-03 21:33:23 +0530

    LRU Cache Example
    Commit for implement LRU cache via LinkedHashMap

---
+++ LRU Cache/src/com/lru/main/LRUCacheExample.java
@@ -0,0 +1,47 @@
+package com.lru.main;
+
+import java.util.LinkedHashMap;
+import java.lang.*;
+
+
+public class LRUCacheExample<String, Integer> extends LinkedHashMap<String, Integer>{
+	
+	
+	private int capacity;
+
+	public LRUCacheExample(int capacity) {
+		super(capacity, 0.5f, true);
+		this.capacity = capacity;
+	} 
+	
+	
+	@Override
+	protected boolean removeEldestEntry(java.util.Map.Entry<String, Integer> eldest) {
+		
+		System.out.println("removeEldestEntry : " + (size() > capacity));
+		return size() > capacity;
+	}
+	
+	
+	public static void main(java.lang.String[] args) {
+		
+		LRUCacheExample<java.lang.String, java.lang.Integer> lruCacheExample = new LRUCacheExample<>(3);
+		
+		lruCacheExample.put("RAM", 40);
+		lruCacheExample.put("John", 90);
+		lruCacheExample.put("RAM", 60);
+		lruCacheExample.get("John");
+		lruCacheExample.put("Rohit", 45);
+		lruCacheExample.put("Virat", 60);
+		
+		
+		System.out.println(lruCacheExample);
+		
+	}
+	
+	
+	
+	
+	
+
+}
